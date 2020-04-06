98526317ed83989758b0d61d7a0cfe43251085fe
HugoNguyen <nguyenquoctuan61089@gmail.com>
2020-04-06 15:45:05 +0700

    Configuring the header bar
    https://reactnavigation.org/docs/headers

--- App.js
+++ App.js
@@ -11,8 +11,31 @@ const Stack = createStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomePage} options={{ title: 'My home' }} />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#f4511e',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}
+      >
+        <Stack.Screen 
+          name="Home" 
+          component={HomePage}
+          options={{
+            title: 'My home',
+            headerStyle: {
+              backgroundColor: '#6a51ae',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+        />
         <Stack.Screen name="Details" component={DetailPage} />
       </Stack.Navigator>
     </NavigationContainer>
